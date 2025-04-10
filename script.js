cf36a2ca733cca171a721ad9e178392dd4c44302
Notabletoad <marvelstudios872@gmail.com>
2025-04-10 12:58:32 +0200

    ordentlich 10

--- script.js
+++ script.js
@@ -147,3 +147,15 @@ function closeInfo() {
             }
         }
     }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropdown-btn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.style.display === "block") {
+                    openDropdown.style.display = "none";
+                }
+            }
+        }
+    }
